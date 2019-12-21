488730faf94ad16e3061d9c4fc730fcf0de49c46
royan2024 <ean2024@gmail.com>
2019-12-21 17:20:30 +0900

    timer

--- Real/Enemy.py
+++ Real/Enemy.py
@@ -5,48 +5,48 @@ from Character import *
 
 
 class Enemy:
-   def __init__(self, x, y, hp=5, size=10, attack_speed=0.3):
-       self.x = x
-       self.y = y
-       self.size = size
-       self.attack_speed = attack_speed
-       self.hp = hp
-       self.visible = True
-       self.acc = (random.randint(-6000, 6000), random.randint(-6000, 6000))
-       self.sprite = arcade.Sprite("resources/tq.png", float(1/33))
-       self.sprite.position = (self.x, self.y)
+    def __init__(self, x, y, hp=5, size=10, attack_speed=0.3):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.attack_speed = attack_speed
+        self.hp = hp
+        self.visible = True
+        self.acc = (random.randint(-6000, 6000), random.randint(-6000, 6000))
+        self.sprite = arcade.Sprite("resources/tq.png", float(1/33))
+        self.sprite.position = (self.x, self.y)
 
-   def fire(self):
-       bullets = []
-       directions = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
-       for i in range(8):
-           bullets.append(Bullet.create_normal_bullet(self.x, self.y, directions[i], True))
-       return bullets
+    def fire(self):
+        bullets = []
+        directions = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
+        for i in range(8):
+            bullets.append(Bullet.create_normal_bullet(self.x, self.y, directions[i], True))
+        return bullets
 
 
-   def draw(self):
-       if self.visible:
-           self.sprite.draw()
-           #arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size, arcade.color.RED)
+    def draw(self):
+        if self.visible:
+            self.sprite.draw()
+            #arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size, arcade.color.RED)
 
-   def update(self, delta_time):
-       self.x += 0.5 * self.acc[0] * delta_time**2
-       self.y += 0.5 * self.acc[1] * delta_time**2
-       if self.x > SCREEN_WIDTH:
-           self.x = SCREEN_WIDTH
-           self.acc = (self.acc[0] * -1, self.acc[1])
+    def update(self, delta_time):
+        self.x += 0.5 * self.acc[0] * delta_time**2
+        self.y += 0.5 * self.acc[1] * delta_time**2
+        if self.x > SCREEN_WIDTH:
+            self.x = SCREEN_WIDTH
+            self.acc = (self.acc[0] * -1, self.acc[1])
 
-       if self.x < 0:
-           self.x = 0
-           self.acc = (self.acc[0] * -1, self.acc[1])
+        if self.x < 0:
+            self.x = 0
+            self.acc = (self.acc[0] * -1, self.acc[1])
 
-       if self.y > SCREEN_HEIGHT:
-           self.y = SCREEN_HEIGHT
-           self.acc = (self.acc[0], self.acc[1] * -1)
-       if self.y < 0:
-           self.y = 0
-           self.acc = (self.acc[0], self.acc[1] * -1)
-       self.sprite.position = (self.x, self.y)
+        if self.y > SCREEN_HEIGHT:
+            self.y = SCREEN_HEIGHT
+            self.acc = (self.acc[0], self.acc[1] * -1)
+        if self.y < 0:
+            self.y = 0
+            self.acc = (self.acc[0], self.acc[1] * -1)
+        self.sprite.position = (self.x, self.y)
 
 #accelration 뒤집기
 
--- Real/Game.py
+++ Real/Game.py
@@ -11,141 +11,147 @@ from Constants import *
 from Character import Character
 from Configure import *
 from Enemy import Enemy
+from Timer import Timer
 
 class Game(arcade.Window):
-   def __init__(self):
-       super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Shooting Game")
-       self.enemies = []
-       for i in range(5):
-           x = random.randint(20, 780)
-           y = random.randint(320, 580)
-           enemy = Enemy(x, y)
-           self.enemies.append(enemy)
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Shooting Game")
+        self.enemies = []
+        for i in range(5):
+            x = random.randint(20, 780)
+            y = random.randint(320, 580)
+            enemy = Enemy(x, y)
+            self.enemies.append(enemy)
 
-       self.character = Character(400, 50)
-       self.pressed = []
-       self.mouse_position = (0, 0)
+        self.character = Character(400, 50)
+        self.pressed = []
+        self.mouse_position = (0, 0)
 
-       self.bullets = []
-       self.recent_fire = 0
-       self.enemy_bullets = []
-       self.recent_enemy_fire = 0
-       self.time = 0
-       self.finished = False
+        self.bullets = []
+        self.recent_fire = 0
+        self.enemy_bullets = []
+        self.recent_enemy_fire = 0
+        self.timer = Timer(10, SCREEN_HEIGHT - 30)
+        self.time = 0
+        self.finished = False
 
-   def on_draw(self):
-       arcade.start_render()
-       for bullet in self.enemy_bullets:
-           bullet.draw()
-       for enemy in self.enemies:
-           enemy.draw()
-       self.character.draw()
-       for bullet in self.bullets:
-           bullet.draw()
+    def on_draw(self):
+        arcade.start_render()
+        for bullet in self.enemy_bullets:
+            bullet.draw()
+        for enemy in self.enemies:
+            enemy.draw()
+        self.character.draw()
+        for bullet in self.bullets:
+            bullet.draw()
+        self.timer.draw()
 
 
-   def on_update(self, delta_time: float):
-       delta_x = 0
-       delta_y = 0
-       if not self.finished:
-           if check_pressed("left", self.pressed):
-               delta_x -= 5
-           if check_pressed("right", self.pressed):
-               delta_x += 5
-           if check_pressed("up", self.pressed):
-               delta_y += 5
-           if check_pressed("down", self.pressed):
-               delta_y -= 5
-           self.character.update(delta_x, delta_y)
+    def on_update(self, delta_time: float):
+        delta_x = 0
+        delta_y = 0
+        if not self.finished:
+            if check_pressed("left", self.pressed):
+                delta_x -= 5
+            if check_pressed("right", self.pressed):
+                delta_x += 5
+            if check_pressed("up", self.pressed):
+                delta_y += 5
+            if check_pressed("down", self.pressed):
+                delta_y -= 5
+            self.character.update(delta_x, delta_y)
 
-       for bullet in self.bullets:
-           bullet.update(delta_time)
+        for bullet in self.bullets:
+            bullet.update(delta_time)
 
-       for bullet in self.enemy_bullets:
-           bullet.update(delta_time)
+        for bullet in self.enemy_bullets:
+            bullet.update(delta_time)
 
-       if not self.finished:
-           for enemy in self.enemies:
-               enemy.update(delta_time)
+        if not self.finished:
+            for enemy in self.enemies:
+                enemy.update(delta_time)
 
-       self.time += delta_time
-       if check_pressed("shoot", self.pressed) and not self.finished:
-           if self.recent_fire == 0 or self.recent_fire + self.character.attack_speed < self.time:
-               x = self.character.x
-               y = self.character.y + 15
-               bullet = Bullet.create_normal_bullet(x, y, (self.mouse_position[0] - x, self.mouse_position[1] - y))
-               self.bullets.append(bullet)
-               self.recent_fire = self.time
+        if not self.finished:
+            self.timer.update(delta_time)
 
-       #지금은 적 fire가 임시구현이라 하드코딩
-       if not self.finished:
-           if self.recent_enemy_fire + 1 < self.time:
-               self.recent_enemy_fire = self.time
-               for enemy in self.enemies:
-                   if enemy.visible:
-                       self.enemy_bullets.extend(enemy.fire())
+        self.time += delta_time
+        if check_pressed("shoot", self.pressed) and not self.finished:
+            if self.recent_fire == 0 or self.recent_fire + self.character.attack_speed < self.time:
+                x = self.character.x
+                y = self.character.y + 15
+                bullet = Bullet.create_normal_bullet(x, y, (self.mouse_position[0] - x, self.mouse_position[1] - y))
+                self.bullets.append(bullet)
+                self.recent_fire = self.time
 
-       self.on_collide()
+        #지금은 적 fire가 임시구현이라 하드코딩
+        if not self.finished:
+            if self.recent_enemy_fire + 1 < self.time:
+                self.recent_enemy_fire = self.time
+                for enemy in self.enemies:
+                    if enemy.visible:
+                        self.enemy_bullets.extend(enemy.fire())
 
-       delete_list = []
-       for i, bullet in enumerate(self.bullets):
-           if bullet.y > 600:
-               delete_list.insert(0, i)
-       for i in delete_list:
-           bullet = self.bullets[i]
-           self.bullets.pop(i)
-           del bullet
+        self.on_collide()
 
-       delete_list = []
-       for i, bullet in enumerate(self.enemy_bullets):
-           if bullet.x > 800 or bullet.x < 0 or\
-                   bullet.y > 600 or bullet.y < 0:
-               delete_list.insert(0, i)
-       for i in delete_list:
-           bullet = self.enemy_bullets[i]
-           self.enemy_bullets.pop(i)
-           del bullet
+        delete_list = []
+        for i, bullet in enumerate(self.bullets):
+            if bullet.y > 600:
+                delete_list.insert(0, i)
+        for i in delete_list:
+            bullet = self.bullets[i]
+            self.bullets.pop(i)
+            del bullet
 
-   def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
-       self.mouse_position = (x, y)
+        delete_list = []
+        for i, bullet in enumerate(self.enemy_bullets):
+            if bullet.x > 800 or bullet.x < 0 or \
+                    bullet.y > 600 or bullet.y < 0:
+                delete_list.insert(0, i)
+        for i in delete_list:
+            bullet = self.enemy_bullets[i]
+            self.enemy_bullets.pop(i)
+            del bullet
 
-   def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
-       self.pressed.append(button)
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        self.mouse_position = (x, y)
 
-   def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
-       self.pressed.remove(button)
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        self.pressed.append(button)
 
-   def on_key_press(self, symbol: int, modifiers: int):
-       self.pressed.append(symbol)
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
+        self.pressed.remove(button)
 
-   def on_key_release(self, symbol: int, modifiers: int):
-       self.pressed.remove(symbol)
+    def on_key_press(self, symbol: int, modifiers: int):
+        self.pressed.append(symbol)
 
-   def on_collide(self):
-       for enemy in self.enemies:
-           for bullet in self.bullets:
-               if abs(bullet.x - enemy.x) < bullet.size + enemy.size / 2 \
-                       and abs(bullet.y - enemy.y) < bullet.size + enemy.size / 2 \
-                       and enemy.visible\
-                       and bullet.visible:
-                   enemy.hp -= 1
-                   if enemy.hp == 0:
-                       enemy.visible = False
-                   bullet.visible = False
+    def on_key_release(self, symbol: int, modifiers: int):
+        self.pressed.remove(symbol)
 
-       for ebullet in self.enemy_bullets:
-           if abs(ebullet.x - self.character.x) < ebullet.size + self.character.size / 2 \
-                       and abs(ebullet.y - self.character.y) < ebullet.size + self.character.size / 2:
-               ebullet.visible = False
-               self.character.visible = False
-               self.finished = True
+    def on_collide(self):
+        for enemy in self.enemies:
+            for bullet in self.bullets:
+                if abs(bullet.x - enemy.x) < bullet.size + enemy.size / 2 \
+                        and abs(bullet.y - enemy.y) < bullet.size + enemy.size / 2 \
+                        and enemy.visible \
+                        and bullet.visible:
+                    enemy.hp -= 1
+                    if enemy.hp == 0:
+                        enemy.visible = False
+                    bullet.visible = False
+
+        for ebullet in self.enemy_bullets:
+            if abs(ebullet.x - self.character.x) < ebullet.size + self.character.size / 2 \
+                    and abs(ebullet.y - self.character.y) < ebullet.size + self.character.size / 2:
+                ebullet.visible = False
+                self.character.visible = False
+                self.finished = True
 
 
 
 
 
 if __name__ == "__main__":
-   Game()
-   arcade.set_background_color(arcade.color.WHITE)
-   arcade.run()
+    Game()
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.run()
 
